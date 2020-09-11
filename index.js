6b221eabb5a7d277149960184e4b2abd87c6b8ae
Deepak Kumar <deepakbedwalb5@gmail.com>
2020-09-11 23:28:17 +0530

    Routing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url'); //used to analyze url
 /*---- FILES ----*/
 
 // blocking synchronous way to read a file 
@@ -29,8 +30,26 @@ const http = require('http');
 /*---- SERVER ----*/
 
 const server = http.createServer((req, res) => {
-    console.log(req);
-    console.log("Hello from the server");
+    //console.log(req); //to show the request details
+    //console.log(req.url); //to show the actual url
+
+    const pathName = req.url;
+
+    if(pathName === '/' || pathName === '/overview'){
+        res.end("This is the OVERVIEW");
+    }
+    else if(pathName === '/product'){
+        res.end("This is the PRODUCT");
+    }
+    else{
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'Hello world'
+        });
+        res.end("<h1>Page not found</h1>");
+    }
+    //console.log("Hello from the server");
+    console.log(res);
 }); 
 
 server.listen(3000, '127.0.0.1', () => {
