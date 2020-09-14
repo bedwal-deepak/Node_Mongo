51da8ffcd8e711276d2899a945e0ebeec9a14fb0
Deepak Kumar <deepakbedwalb5@gmail.com>
2020-09-14 22:10:44 +0530

    HTML templating

--- index.js
+++ index.js
@@ -1,57 +1,60 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url'); //used to analyze url
-/*---- FILES ----*/
-
-// blocking synchronous way to read a file 
-// const textIn = fs.readFileSync('./input.txt', 'utf-8');
-// console.log(textIn);
-
-// const textOut = `This is regarding the write file using sync file system: ${textIn}. \n Created on:- ${Date.now()}`;
-// fs.writeFileSync('./output.txt', textOut);
-
-// Non-blocking asynchronous way to read a file
-// fs.readFile('./start.txt', 'utf-8', (err, data1) =>{
-//     //if(err) return console.log('ERROR!!!!')
-//     console.log(data1);
-//     fs.readFile(`./${data1}.txt`, 'utf-8', (err, data2) =>{
-//         console.log(data2);
-//         fs.readFile('./append.txt', 'utf-8', (err, data3) =>{
-//             console.log(data3);
-
-//             fs.writeFile('./final.txt', `${data2} \n${data3}`, 'utf-8', err => {
-//                 console.log('File is written');
-//             });
-//         });
-//     });
-// });
-// console.log('Will read this file!!!');
 
 /*---- SERVER ----*/
 
 //top level code, it will executed once when we start the program
 //in case when we read the same file data on each request
+
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName); //here g is used to declare global
+    output = output.replace(/{%IMAGE%}/g, product.image); //its not good practice to change directly the parameter
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DISCRIPTION%}/g, product.discription);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if(!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8'); 
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8'); 
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8'); 
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8'); 
 const dataObj = JSON.parse(data);   
 
 const server = http.createServer((req, res) => {
-    //console.log(req); //to show the request details
-    //console.log(req.url); //to show the actual url
-
     const pathName = req.url;
 
+    //OVERVIEW PAGE
     if(pathName === '/' || pathName === '/overview'){
-        res.end("This is the OVERVIEW");
+        res.writeHead(200, {'Content-type': 'text/html'}); 
+
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join();
+        // console.log(cardsHtml);
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+        res.end(output);
     }
+
+    //PRODUCT PAGE
     else if(pathName === '/product'){
         res.end("This is the PRODUCT");
     }
+
+    //API
     else if(pathName === '/api'){
-            res.writeHead(200, {
-                'Content-type': 'application/json'
-            }); 
+            res.writeHead(200, {'Content-type': 'application/json'}); 
             res.end(data);
     }
+
+    //NOT FOUND
     else{
         res.writeHead(404, {
             'Content-type': 'text/html',
@@ -59,8 +62,6 @@ const server = http.createServer((req, res) => {
         });
         res.end("<h1>Page not found</h1>");
     }
-    //console.log("Hello from the server");
-   // console.log(res);
 }); 
 
 server.listen(3000, '127.0.0.1', () => {
